242e30b76c40a3541397857249e46c41c693abf6
Sujan <sujangraduate162004@gmail.com>
2024-10-20 12:03:32 +0530

    Added somemorefiles

---
+++ add.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 3,y=4;
+    int z = x+y;
+    cout << "Sum of 3 and 4 is: "+z << endl;
+}
---
+++ add.java
@@ -0,0 +1,7 @@
+class add{
+    public static void main(String[] args) {
+        int x = 3,y=4;
+        int z = x+y;
+        System.out.println("Sum of 3 and 4 is: "+z);
+    }
+}
